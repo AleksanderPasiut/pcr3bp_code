ad9b348f839b8cd88da3d90bb5f3cd6e4684d2e6
Aleksander Pasiut <apasiut@protonmail.com>
2023-07-20 19:14:11 +0200

    rework GIG map

--- src/proof/g_map.hpp
+++ src/proof/g_map.hpp
@@ -34,27 +34,28 @@ public:
                 order,
                 src_coordsys,
                 dst_coordsys)
-            , m_gig(m_local_poincare4, input_gain)
+            , m_input_gain(input_gain, m_local_poincare4.dimension())
+            , m_output_gain(ScalarType(1.0) / input_gain, m_local_poincare4.imageDimension())
     {}
 
     VectorType operator() (const VectorType& vec) override
     {
-        return m_gig(vec);
+        return m_composite(vec);
     }
 
     VectorType operator() (const VectorType& vec, MatrixType& der) override
     {
-        return m_gig(vec, der);
+        return m_composite(vec, der);
     }
 
     unsigned dimension() const override
     {
-        return m_gig.dimension();
+        return m_composite.dimension();
     }
 
     unsigned imageDimension() const override
     {
-        return m_gig.imageDimension();
+        return m_composite.imageDimension();
     }
 
     ScalarType get_last_evaluation_return_time() const
@@ -62,19 +63,35 @@ public:
         return m_local_poincare4.get_last_evaluation_return_time();
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //! @brief Get Poincare time and solution curve of the underlying Poincare map
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void operator() (const VectorType& vec, ScalarType time, Carina::SolutionCurve<MapT>& solution_curve)
     {
-        return m_local_poincare4(vec * get_input_gain(), time, solution_curve);
+        const ScalarType k = m_input_gain.get_gain();
+        return m_local_poincare4(vec * k, time, solution_curve);
     }
 
-    ScalarType get_input_gain()
-    {
-        return m_gig.get_input_gain();
-    }
+    // ScalarType get_input_gain()
+    // {
+    //     return m_input_gain.get_gain();
+    // }
 
 private:
     LocalPoincare4<MapT> m_local_poincare4;
-    Carina::GigMap<MapT, decltype(m_local_poincare4)> m_gig;
+    Carina::GainMap<MapT> m_input_gain;
+    Carina::GainMap<MapT> m_output_gain;
+
+    Carina::CompositeMap<
+        MapT,
+        decltype(m_input_gain)&,
+        decltype(m_local_poincare4)&,
+        decltype(m_output_gain)&> m_composite
+    {
+        std::ref(m_input_gain),
+        std::ref(m_local_poincare4),
+        std::ref(m_output_gain)
+    };
 };
 
 }
--- src/tools/gig_map.hpp
+++ src/tools/gig_map.hpp
@@ -5,13 +5,66 @@
 #pragma once
 
 #include <carina/map_base.hpp>
-#include <carina/map_compatibility.hpp>
+#include <carina/map_compatibility.hpp> // to be removed
 
 namespace Carina
 {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//! @brief GIG map implementation
+//! @brief Gain map implementation
+//! @details Define function G(x) = k * x, where k is some real coefficient.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename MapT>
+class GainMap : public MapBase<MapT>
+{
+public:
+    using ScalarType = typename MapT::ScalarType;
+    using VectorType = typename MapT::VectorType;
+    using MatrixType = typename MapT::MatrixType;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //! @brief Constructor
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    GainMap(ScalarType gain, unsigned dimension) : m_gain(gain), m_dimension(dimension)
+    {}
+
+    VectorType operator() (const VectorType& vec) override
+    {
+        this->assert_vector_size(vec, dimension(), "GainMap vec vector size mismatch (1)!");
+
+        return vec * m_gain;
+    }
+
+    VectorType operator() (const VectorType& vec, MatrixType& mat) override
+    {
+        this->assert_vector_size(vec, dimension(), "GainMap vec vector size mismatch (2)!");
+
+        mat = MatrixType::Identity(m_dimension) * m_gain;
+        return vec * m_gain;
+    }
+
+    unsigned dimension() const noexcept override
+    {
+        return m_dimension;
+    }
+
+    unsigned imageDimension() const noexcept override
+    {
+        return m_dimension;
+    }
+
+    ScalarType get_gain() const
+    {
+        return m_gain;
+    }
+
+private:
+    const ScalarType m_gain;
+    const unsigned m_dimension;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! @brief GIG (Gain and Inverse Gain) map implementation
 //! @details Define function g(x) = 1/z f(z*x) for arbitrary function f, where z is input_gain value.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 template<typename MapT, typename MapU>
