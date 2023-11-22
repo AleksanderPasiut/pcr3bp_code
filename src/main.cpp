6f1e0e8a51749300b75d8548261bcae68e531754
Aleksander Pasiut <apasiut@protonmail.com>
2023-11-22 22:53:54 +0100

    remove commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,5 @@
 // Author: Aleksander M. Pasiut
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// #include "plot_1/std_evolution_raw_master.hpp"
-// #include "plot_1/std_evolution_lyapunov_master.hpp"
-
-// #include "plot_1/reg_evolution_raw_master.hpp"
-// #include "plot_1/reg_evolution_lyapunov_master.hpp"
 #include "plot_1/reg_evolution_lyapunov_master.multi.hpp"
 #include "plot_1/reg_evolution_lyapunov_master.multi.std.hpp"
-// #include "plot_1/reg_evolution_xieta_master.hpp"
-
-// #include "plot_1/rhez_u_master.hpp"
-// #include "plot_1/rhez_u_34_master.hpp"
-
-// #include "plot_2/rhez_u_24_master.hpp"
-// #include "plot_2/gen2dplot_master.hpp"
-
-// #include "plot_2/gpg_master.hpp"
-// #include "plot_2/coverings_master.hpp"
