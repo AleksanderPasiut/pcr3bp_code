961d4f0bc903977805b28410a0851236cc8ab7c9
Aleksander Pasiut <apasiut@protonmail.com>
2023-11-22 20:15:31 +0100

    Revert "remove basic tests and covering relation export"
    This reverts commit ed3ad4673b05cfe6943f73621df89c8df439f332.

---
+++ src/pcr3bp_basic_test/levi_civita_coordinate_change_test.cpp
@@ -0,0 +1,207 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Author: Aleksander M. Pasiut
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <random>
+
+#include "tools/test_tools.hpp"
+#include "pcr3bp_basic/levi_civita_coordinate_change.hpp"
+#include "pcr3bp_basic/levi_civita_inverse_coordinate_change.hpp"
+
+namespace Ursa
+{
+
+class LeviCivitaCoordinateChangeTest
+{
+public:
+    using MapT = IMap;
+    using ScalarType = typename MapT::ScalarType;
+    using VectorType = typename MapT::VectorType;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //! @brief Perform coordinate change from standard to regularized and back and compare the results.
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void check_coordinate_change_to_reg_and_back(const VectorType& X, ScalarType precision)
+    {
+        const VectorType U = m_rci(X);
+        const VectorType X2 = m_rcf(U);
+        EXPECT_LT(m_norm(X - X2), precision);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //! @brief Perform coordinate change from regularized to standard and back and compare the results.
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void check_coordinate_change_to_std_and_back(const VectorType& U, ScalarType precision)
+    {
+        const VectorType X = m_rcf(U);
+        const VectorType U2 = m_rci(X);
+        EXPECT_LT(m_norm(U - U2), precision);
+    }
+
+private:
+    Pcr3bp::SetupParameters<MapT> m_setup { ScalarType(1) / ScalarType(10) };
+    
+    LeviCivitaInverseCoordinateChange<MapT> m_rci{ m_setup.get_x(2) };
+    MapT m_rcf { LeviCivitaCoordinateChange<MapT>::create(2, m_setup, true, true, false) };
+
+    Carina::MaxNorm<MapT> m_norm {};
+};
+
+}
+
+TEST(Pcr3bp_basic, coordinate_change_std_to_reg_and_back)
+{
+    using namespace Ursa;
+
+    std::mt19937_64 rng {};
+    std::normal_distribution<double> dist {};
+
+    LeviCivitaCoordinateChangeTest test {};
+
+    for (int i = 0; i < 100;)
+    {
+        const double x = dist(rng);
+        const double y = dist(rng);
+
+        if ((x+0.1)*(x+0.1) + y*y > 0.0025)
+        {
+            const double px = dist(rng);
+            const double py = dist(rng);
+            test.check_coordinate_change_to_reg_and_back({ x, y, px, py, 0.0 }, 4.1e-12);
+            ++i;
+        }
+    }
+}
+
+TEST(Pcr3bp_basic, coordinate_change_reg_to_std_and_back)
+{
+    using namespace Ursa;
+
+    std::mt19937_64 rng {};
+    std::normal_distribution<double> dist {};
+
+    LeviCivitaCoordinateChangeTest test {};
+
+    for (int i = 0; i < 100;)
+    {
+        const double u = std::abs(dist(rng));
+        const double v = dist(rng);
+
+        if (u > 0.05)
+        {
+            const double pu = dist(rng);
+            const double pv = dist(rng);
+            test.check_coordinate_change_to_std_and_back({ u, v, pu, pv, 0.0 }, 3.2e-12);
+            ++i;
+        }
+    }
+}
+
+TEST(Pcr3bp_basic, coordinate_change_test)
+{
+    using namespace Ursa;
+
+    std::mt19937_64 rng {};
+    std::normal_distribution<double> dist {};
+
+    using MapT = IMap;
+    using ScalarType = typename MapT::ScalarType;
+    using VectorType = typename MapT::VectorType;
+
+    Pcr3bp::SetupParameters<MapT> m_setup { ScalarType(1) / ScalarType(10) };
+
+    MapT m_lcf { LeviCivitaCoordinateChange<MapT>::create(2, m_setup, true, false, false) };
+
+    Carina::MaxNorm<MapT> m_norm {};
+
+    auto test = [m_norm, m_lcf](MapT m_lci, VectorType U, ScalarType precision)
+    {
+        const VectorType X = m_lcf(U);
+        const VectorType U2 = m_lci(X);
+        EXPECT_LT(m_norm(U - U2), precision);
+    };
+
+    {
+        SCOPED_TRACE("PositiveU test");
+        MapT m_lci { LeviCivitaCoordinateChange<MapT>::createInverse(2, m_setup, true, false, LeviCivitaCoordinateChangeInverseVariant::PositiveU ) };
+
+        for (int i = 0; i < 100;)
+        {
+            const double u = std::abs( dist(rng) );
+            const double v = dist(rng);
+
+            if (u > 0.05)
+            {
+                const double pu = dist(rng);
+                const double pv = dist(rng);
+                const VectorType U = { u, v, pu, pv };
+
+                test(m_lci, U, 7.5e-14);
+                ++i;
+            }
+        }
+    }
+
+    {
+        SCOPED_TRACE("NegativeU test");
+        MapT m_lci { LeviCivitaCoordinateChange<MapT>::createInverse(2, m_setup, true, false, LeviCivitaCoordinateChangeInverseVariant::NegativeU ) };
+
+        for (int i = 0; i < 100;)
+        {
+            const double u = -std::abs( dist(rng) );
+            const double v = dist(rng);
+
+            if (-u > 0.05)
+            {
+                const double pu = dist(rng);
+                const double pv = dist(rng);
+                const VectorType U = { u, v, pu, pv };
+
+                test(m_lci, U, 7.5e-14);
+                ++i;
+            }
+        }
+    }
+
+    {
+        SCOPED_TRACE("PositiveV test");
+        MapT m_lci { LeviCivitaCoordinateChange<MapT>::createInverse(2, m_setup, true, false, LeviCivitaCoordinateChangeInverseVariant::PositiveV ) };
+
+        for (int i = 0; i < 100;)
+        {
+            const double v = std::abs( dist(rng) );
+            const double u = dist(rng);
+
+            if (v > 0.05)
+            {
+                const double pu = dist(rng);
+                const double pv = dist(rng);
+                const VectorType U = { u, v, pu, pv };
+
+                test(m_lci, U, 2.9e-14);
+                ++i;
+            }
+        }
+    }
+
+    {
+        SCOPED_TRACE("NegativeV test");
+        MapT m_lci { LeviCivitaCoordinateChange<MapT>::createInverse(2, m_setup, true, false, LeviCivitaCoordinateChangeInverseVariant::NegativeV ) };
+
+        for (int i = 0; i < 100;)
+        {
+            const double v = -std::abs( dist(rng) );
+            const double u = dist(rng);
+
+            if (-v > 0.05)
+            {
+                const double pu = dist(rng);
+                const double pv = dist(rng);
+                const VectorType U = { u, v, pu, pv };
+
+                test(m_lci, U, 2.7e-13);
+                ++i;
+            }
+        }
+    }
+}
---
+++ src/proof/covering_relations_setup_export.cpp
@@ -0,0 +1,174 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Author: Aleksander M. Pasiut
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "tools/test_tools.hpp"
+#include "covering_relations_setup.hpp"
+
+#include "tools/floating_info.hpp"
+
+namespace Ursa
+{
+
+template<unsigned component_offset, typename LabelFunc>
+void print_vector_list_tex(std::ostream& ostr, const std::list<IVector>& args, LabelFunc label_func)
+{
+    ostr << "\\renewcommand{\\arraystretch}{1.2}\n\n";
+    ostr << "\\small\n\n";
+    ostr << "\\begin{tabular}{ c | c | c }\n\n";
+    ostr << "Vector & Component " << component_offset+1 << " & Component " << component_offset+2 << " \\\\\n\n";
+    ostr << "\\hline\n\n";
+    int index = 0;
+    for (const IVector& vec : args)
+    {
+        ostr << "$";
+        label_func(ostr, index);
+        ostr << "$ & ";
+
+        const Interval v0 = vec[component_offset];
+        const Interval v1 = vec[component_offset+1];
+        ostr << "$\\big[" << Carina::FloatingInfo<double>(v0.leftBound()) << ", " << Carina::FloatingInfo<double>(v0.rightBound()) << "\\big]$ & ";
+        ostr << "$\\big[" << Carina::FloatingInfo<double>(v1.leftBound()) << ", " << Carina::FloatingInfo<double>(v1.rightBound()) << "\\big]$ \\\\\n\n";
+        ++index;
+    }
+    ostr << "\\end{tabular}\n\n";
+}
+
+template<typename LabelFunc>
+void print_vector_list_tex(std::ostream& ostr, const std::list<RVector>& args, LabelFunc label_func)
+{
+    ostr << "\\renewcommand{\\arraystretch}{1.2}\n\n";
+    ostr << "\\small\n\n";
+    ostr << "\\begin{tabular}{ c | c | c | c | c }\n\n";
+    ostr << "Vector & Component 1 & Component 2 & Component 3 & Component 4 \\\\\n\n";
+    ostr << "\\hline\n\n";
+    int index = 0;
+    for (const RVector& vec : args)
+    {
+        ostr << "$";
+        label_func(ostr, index);
+        ostr << "$ & ";
+        ostr << "$" << Carina::FloatingInfo<double>(vec[0]) << "$ & ";
+        ostr << "$" << Carina::FloatingInfo<double>(vec[1]) << "$ & ";
+        ostr << "$" << Carina::FloatingInfo<double>(vec[2]) << "$ & ";
+        ostr << "$" << Carina::FloatingInfo<double>(vec[3]) << "$ \\\\\n\n";
+
+        ++index;
+    }
+    ostr << "\\end{tabular}\n\n";
+}
+
+template<unsigned component_offset, typename LabelFunc>
+void print_vector_list_tex(std::string filename, const std::list<IVector>& args, LabelFunc label_func)
+{
+    std::ofstream ostr(filename);
+
+    if (ostr)
+    {
+        print_vector_list_tex<component_offset>(ostr, args, label_func);
+        ostr.close();
+    }
+}
+
+template<typename LabelFunc>
+void print_vector_list_tex(std::string filename, const std::list<RVector>& args, LabelFunc label_func)
+{
+    std::ofstream ostr(filename);
+
+    if (ostr)
+    {
+        print_vector_list_tex(ostr, args, label_func);
+        ostr.close();
+    }
+}
+
+TEST(Pcr3bp_proof, export_covering_relations_setup_data)
+{
+    using namespace Ursa;
+
+    capd::rounding::DoubleRounding::roundNearest();
+
+    CoveringRelationsSetup setup {};
+
+    using Coordsys = Carina::LocalCoordinateSystem<IMap>;
+
+    std::list<IVector> periodic_orbit_origins {};
+    std::list<RVector> homoclinic_orbit_origins {};
+    std::array<std::list<RVector>, 4> coordsys_directions {};
+
+    for (Coordsys coordsys : setup.get_periodic_orbit_coordsys())
+    {
+        periodic_orbit_origins.emplace_back( coordsys.get_origin() );
+        
+        const std::array<IVector, 4> directions_iv
+        {
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 1),
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 2),
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 3),
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 4)
+        };
+
+        for (size_t dir_idx = 0; dir_idx < directions_iv.size(); ++dir_idx)
+        {
+            const IVector direction_iv = directions_iv.at(dir_idx);
+            assert_with_exception(Carina::span_vector(direction_iv) == RVector(4));
+
+            coordsys_directions.at(dir_idx).emplace_back( Carina::vector_cast<RVector>(direction_iv) );
+        }
+    };
+
+    for (Coordsys coordsys : setup.get_homoclinic_orbit_coordsys())
+    {
+        const IVector origin_iv = coordsys.get_origin();
+        assert_with_exception(Carina::span_vector(origin_iv) == RVector(4));
+        homoclinic_orbit_origins.emplace_back( Carina::vector_cast<RVector>(origin_iv) );
+
+        const std::array<IVector, 4> directions_iv
+        {
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 1),
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 2),
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 3),
+            Carina::Extract<IMap>::get_vvector(coordsys.get_directions_matrix(), 4)
+        };
+
+        for (size_t dir_idx = 0; dir_idx < directions_iv.size(); ++dir_idx)
+        {
+            const IVector direction_iv = directions_iv.at(dir_idx);
+            assert_with_exception(Carina::span_vector(direction_iv) == RVector(4));
+
+            coordsys_directions.at(dir_idx).emplace_back( Carina::vector_cast<RVector>(direction_iv) );
+        }
+    };
+
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "w_{" << index << "}"; };
+        print_vector_list_tex<0>("periodic_orbit_origins_1.tex.generated", periodic_orbit_origins, label_func);
+    }
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "w_{" << index << "}"; };
+        print_vector_list_tex<2>("periodic_orbit_origins_2.tex.generated", periodic_orbit_origins, label_func);
+    }
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "w_{" << (index+4) << "}"; };
+        print_vector_list_tex("homoclinic_orbit_origins.tex.generated", homoclinic_orbit_origins, label_func);
+    }
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "\\hat{w}^{" << index << "}_1"; };
+        print_vector_list_tex("coordsys_directions_1.tex.generated", coordsys_directions.at(0), label_func);
+    }
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "\\hat{w}^{" << index << "}_2"; };
+        print_vector_list_tex("coordsys_directions_2.tex.generated", coordsys_directions.at(1), label_func);
+    }
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "\\hat{w}^{" << index << "}_3"; };
+        print_vector_list_tex("coordsys_directions_3.tex.generated", coordsys_directions.at(2), label_func);
+    }
+    {
+        auto label_func = [](std::ostream& ostr, unsigned index) { ostr << "\\hat{w}^{" << index << "}_4"; };
+        print_vector_list_tex("coordsys_directions_4.tex.generated", coordsys_directions.at(3), label_func);
+    }
+}
+
+}
+
