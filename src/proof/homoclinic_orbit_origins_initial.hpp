2d35cafa430dc3e80001c802ffd07564a1cc2bdd
Aleksander Pasiut <apasiut@protonmail.com>
2023-12-07 22:36:46 +0100

    update bootstrapped homoclinic origins

--- src/proof/homoclinic_orbit_origins_initial.hpp
+++ src/proof/homoclinic_orbit_origins_initial.hpp
@@ -44,105 +44,105 @@ private:
     std::vector<VectorType> m_points
     {
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3de90e836175052b"),
+            CapdUtils::ReadableScalar<ScalarType>("3de90e827829c88e"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3e2a1acd6a283e1f"),
+            CapdUtils::ReadableScalar<ScalarType>("3e2a1acc6ddb15f9"),
             CapdUtils::ReadableScalar<ScalarType>("40067d3086e3bf09")
         },
         VectorType{
             CapdUtils::ReadableScalar<ScalarType>("3fefeb9cf153305e"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3e999e0cbdc16781"),
-            CapdUtils::ReadableScalar<ScalarType>("c008cd850bceea24")
+            CapdUtils::ReadableScalar<ScalarType>("3e999e0caa2327ae"),
+            CapdUtils::ReadableScalar<ScalarType>("c008cd850bcee9d4")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3e808c01cf69f720"),
+            CapdUtils::ReadableScalar<ScalarType>("3e808c01ef984092"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3ec13fce4f22b9ab"),
-            CapdUtils::ReadableScalar<ScalarType>("40067d30867c8e15")
+            CapdUtils::ReadableScalar<ScalarType>("3ec13fce4ef5736c"),
+            CapdUtils::ReadableScalar<ScalarType>("40067d30867c8da3")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3fefeb9e55517940"),
+            CapdUtils::ReadableScalar<ScalarType>("3fefeb9e5551793e"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3f30ecc9a62dcdb4"),
-            CapdUtils::ReadableScalar<ScalarType>("c008ce91f4d9e33c")
+            CapdUtils::ReadableScalar<ScalarType>("3f30ecc9a62804ac"),
+            CapdUtils::ReadableScalar<ScalarType>("c008ce91f4d9e177")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3f15dd90c3d60050"),
+            CapdUtils::ReadableScalar<ScalarType>("3f15dd90c3f7dc63"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3f56cc4d47f331d5"),
-            CapdUtils::ReadableScalar<ScalarType>("40067d2f46aff031")
+            CapdUtils::ReadableScalar<ScalarType>("3f56cc4d47f312a1"),
+            CapdUtils::ReadableScalar<ScalarType>("40067d2f46afef44")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3fefef30defcc3f7"),
+            CapdUtils::ReadableScalar<ScalarType>("3fefef30defcc3f4"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3fc91deae1af46bb"),
-            CapdUtils::ReadableScalar<ScalarType>("c00bee4375e81fe9")
+            CapdUtils::ReadableScalar<ScalarType>("3fc91deae1af399f"),
+            CapdUtils::ReadableScalar<ScalarType>("c00bee4375e81c19")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3fb304b302710e2e"),
+            CapdUtils::ReadableScalar<ScalarType>("3fb304b302711e56"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3ff3f0b1814005ec"),
-            CapdUtils::ReadableScalar<ScalarType>("400419ea3b06083c")
+            CapdUtils::ReadableScalar<ScalarType>("3ff3f0b1814003a5"),
+            CapdUtils::ReadableScalar<ScalarType>("400419ea3b0606b8")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3ff440d7b8e22ddf"),
+            CapdUtils::ReadableScalar<ScalarType>("3ff440d7b8e22d64"),
             CapdUtils::ReadableScalar<ScalarType>("0000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("bcc7000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("3fbec12bfe6c74df")
+            CapdUtils::ReadableScalar<ScalarType>("3cd1000000000000"),
+            CapdUtils::ReadableScalar<ScalarType>("3fbec12bfe6c916a")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3fb304b302710e2e"),
+            CapdUtils::ReadableScalar<ScalarType>("3fb304b302711e56"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("bff3f0b1814005ec"),
-            CapdUtils::ReadableScalar<ScalarType>("400419ea3b06083c")
+            CapdUtils::ReadableScalar<ScalarType>("bff3f0b1814003a5"),
+            CapdUtils::ReadableScalar<ScalarType>("400419ea3b0606b8")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3fefef30defcc3f7"),
+            CapdUtils::ReadableScalar<ScalarType>("3fefef30defcc3f4"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("bfc91deae1af46bb"),
-            CapdUtils::ReadableScalar<ScalarType>("c00bee4375e81fe9")
+            CapdUtils::ReadableScalar<ScalarType>("bfc91deae1af399f"),
+            CapdUtils::ReadableScalar<ScalarType>("c00bee4375e81c19")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3f15dd90c3d60050"),
+            CapdUtils::ReadableScalar<ScalarType>("3f15dd90c3f7dc63"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("bf56cc4d47f331d5"),
-            CapdUtils::ReadableScalar<ScalarType>("40067d2f46aff031")
+            CapdUtils::ReadableScalar<ScalarType>("bf56cc4d47f312a1"),
+            CapdUtils::ReadableScalar<ScalarType>("40067d2f46afef44")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3fefeb9e55517940"),
+            CapdUtils::ReadableScalar<ScalarType>("3fefeb9e5551793e"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("bf30ecc9a62dcdb4"),
-            CapdUtils::ReadableScalar<ScalarType>("c008ce91f4d9e33c")
+            CapdUtils::ReadableScalar<ScalarType>("bf30ecc9a62804ac"),
+            CapdUtils::ReadableScalar<ScalarType>("c008ce91f4d9e177")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3e808c01cf69f720"),
+            CapdUtils::ReadableScalar<ScalarType>("3e808c01ef984092"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("bec13fce4f22b9ab"),
-            CapdUtils::ReadableScalar<ScalarType>("40067d30867c8e15")
+            CapdUtils::ReadableScalar<ScalarType>("bec13fce4ef5736c"),
+            CapdUtils::ReadableScalar<ScalarType>("40067d30867c8da3")
         },
         VectorType{
             CapdUtils::ReadableScalar<ScalarType>("3fefeb9cf153305e"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("be999e0cbdc16781"),
-            CapdUtils::ReadableScalar<ScalarType>("c008cd850bceea24")
+            CapdUtils::ReadableScalar<ScalarType>("be999e0caa2327ae"),
+            CapdUtils::ReadableScalar<ScalarType>("c008cd850bcee9d4")
         },
         VectorType{
-            CapdUtils::ReadableScalar<ScalarType>("3de90e836175052b"),
+            CapdUtils::ReadableScalar<ScalarType>("3de90e827829c88e"),
             CapdUtils::ReadableScalar<ScalarType>("8000000000000000"),
-            CapdUtils::ReadableScalar<ScalarType>("be2a1acd6a283e1f"),
+            CapdUtils::ReadableScalar<ScalarType>("be2a1acc6ddb15f9"),
             CapdUtils::ReadableScalar<ScalarType>("40067d3086e3bf09")
         }
     };
 
     ScalarType m_total_expansion_factor_pos
     {
-        CapdUtils::ReadableScalar<ScalarType>("438b3581527bf58d")
+        CapdUtils::ReadableScalar<ScalarType>("438b3581527bf5b4")
     };
 
     ScalarType m_total_expansion_factor_neg
     {
-        CapdUtils::ReadableScalar<ScalarType>("438b3581527bf590")
+        CapdUtils::ReadableScalar<ScalarType>("438b3581527bf5c5")
     };
 };
 
--- src/tools/print_bootstrap.cpp
+++ src/tools/print_bootstrap.cpp
@@ -17,7 +17,7 @@ void BootstrapPrint<RMap>::print(std::ostream& ostr, const VectorType& vec)
 
     for (int i = 0; ; ++i)
     {
-        ostr << "\tCarina::ReadableScalar<ScalarType>(\"" << std::string(CapdUtils::ReadableScalar<ScalarType>(vec[i])) << "\")";
+        ostr << "\tCapdUtils::ReadableScalar<ScalarType>(\"" << std::string(CapdUtils::ReadableScalar<ScalarType>(vec[i])) << "\")";
 
         if (i+1 < vec.dimension())
         {
@@ -45,7 +45,7 @@ void BootstrapPrint<RMap>::print(std::ostream& ostr, const MatrixType& mat)
     int i = 1;
     for (auto e : mat)
     {
-        ostr << "\tCarina::ReadableScalar<ScalarType>(\"" << std::string(CapdUtils::ReadableScalar<double>(e)) << "\")";
+        ostr << "\tCapdUtils::ReadableScalar<ScalarType>(\"" << std::string(CapdUtils::ReadableScalar<double>(e)) << "\")";
 
         if (i < count)
         {
