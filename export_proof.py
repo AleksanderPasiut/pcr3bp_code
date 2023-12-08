5e95953052d177ae9b4e6b3a372f8577af91e9d8
Aleksander Pasiut <apasiut@protonmail.com>
2023-12-08 21:34:28 +0100

    fix files removal in export proof script

--- export_proof.py
+++ export_proof.py
@@ -1,18 +1,25 @@
 import os
 import shutil
 
+def remove_file(path):
+    if os.path.exists(path):
+        os.remove(path)
+
 # remove plotting related code
 shutil.rmtree('src/plot_common', ignore_errors=True)
 shutil.rmtree('src/plot_1', ignore_errors=True)
 shutil.rmtree('src/pcr3bp_obsolete', ignore_errors=True)
 shutil.rmtree('src/bluesky', ignore_errors=True)
 shutil.rmtree('src/tools/plotting', ignore_errors=True)
-shutil.rmtree('src/main.cpp', ignore_errors=True)
-shutil.rmtree('src/capd_renderable.hpp', ignore_errors=True)
+remove_file('src/main.cpp')
+remove_file('src/capd_renderable.hpp')
 
 # remove basic test code
 shutil.rmtree('src/pcr3bp_basic_test', ignore_errors=True)
 
+# remove homoclinic initial test
+remove_file('src/proof/homoclinic_orbit_origins_initial_test.cpp')
+
 # replace current CMakeLists.txt
-os.remove('CMakeLists.txt')
+remove_file('CMakeLists.txt')
 shutil.copyfile('CMakeLists.proof_only.txt', 'CMakeLists.txt')
