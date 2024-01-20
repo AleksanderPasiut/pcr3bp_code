cfbcb31a552f00bacedeeaf37942da4b1ff07553
Aleksander Pasiut <apasiut@protonmail.com>
2024-01-20 22:59:24 +0100

    rename CARINA_LOG -> CAPD_UTILS_LOG

--- src/proof/periodic_orbit_parameters_test.hpp
+++ src/proof/periodic_orbit_parameters_test.hpp
@@ -12,9 +12,9 @@
 #include <capd_utils/pne_map.hpp>
 #include <capd_utils/identity_map.hpp>
 #include <capd_utils/readable_interval.hpp>
-#define CARINA_LOG
+#define CAPD_UTILS_LOG
 #include <capd_utils/newton_method/newton_method.hpp>
-#undef CARINA_LOG
+#undef CAPD_UTILS_LOG
 #include <capd_utils/poincare_wrapper.hpp>
 #include <capd_utils/timemap_wrapper.hpp>
 #include <capd_utils/enp_map.hpp>
