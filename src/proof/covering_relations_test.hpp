16ee829f9a3b25e0f832c7e0afc2b37b261c866a
Aleksander Pasiut <apasiut@protonmail.com>
2024-01-20 20:46:27 +0100

    introduce covering relations test base

--- src/proof/covering_relations_test.hpp
+++ src/proof/covering_relations_test.hpp
@@ -9,7 +9,7 @@
 #include "tools/solution_curve_with_condition_check.hpp"
 #include "tools/auxiliary_functions.hpp"
 
-#include "covering_relations_setup.hpp"
+#include "covering_relations_test_base.hpp"
 #include "covering_relation_checker.hpp"
 #include "parallelogram_covering_checker.hpp"
 
@@ -19,7 +19,7 @@ namespace Pcr3bpProof
 {
 
 template<typename MapT>
-class CoveringRelationsTest
+class CoveringRelationsTest : public CoveringRelationsTestBase<MapT>
 {
 public:
     using ScalarType = typename MapT::ScalarType;
@@ -29,8 +29,7 @@ public:
     using Coordsys = CapdUtils::LocalCoordinateSystem<MapT>;
 
     CoveringRelationsTest(const CoveringRelationsSetup& setup)
-        : m_periodic_orbit_coordsys(setup.get_periodic_orbit_coordsys())
-        , m_homoclinic_orbit_coordsys(setup.get_homoclinic_orbit_coordsys())
+        : CoveringRelationsTestBase<MapT>(setup)
     {}
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,14 +37,14 @@ public:
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void check_homoclinic_coverings()
     {
-        for (size_t i = 1; i < m_homoclinic_orbit_coordsys.size(); ++i)
+        for (size_t i = 1; i < this->m_homoclinic_orbit_coordsys.size(); ++i)
         {
             const size_t src_idx = i-1;
             const size_t dst_idx = i;
             std::cout << "homoclinic orbit covering " << src_idx << " => " << dst_idx << '\n';
 
-            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = m_homoclinic_orbit_coordsys.at(src_idx);
-            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = m_homoclinic_orbit_coordsys.at(dst_idx);
+            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = this->m_homoclinic_orbit_coordsys.at(src_idx);
+            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = this->m_homoclinic_orbit_coordsys.at(dst_idx);
 
             const ScalarType time_span = check_covering_relation_forward(coordsys_src, coordsys_dst);
             simple_collision_avoidance_check(coordsys_src, coordsys_dst, time_span);
@@ -60,16 +59,16 @@ public:
         {
             std::cout << "periodic orbit covering 0 => 1\n";
 
-            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = m_periodic_orbit_coordsys.at(0);
-            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = m_periodic_orbit_coordsys.at(1);
+            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = this->m_periodic_orbit_coordsys.at(0);
+            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = this->m_periodic_orbit_coordsys.at(1);
             check_covering_relation_forward(coordsys_src, coordsys_dst, true);
         }
 
         {
             std::cout << "periodic orbit covering 1 => 2\n";
 
-            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = m_periodic_orbit_coordsys.at(1);
-            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = m_periodic_orbit_coordsys.at(2);
+            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = this->m_periodic_orbit_coordsys.at(1);
+            const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = this->m_periodic_orbit_coordsys.at(2);
             const ScalarType time_span = check_covering_relation_forward(coordsys_src, coordsys_dst);
             simple_collision_avoidance_check(coordsys_src, coordsys_dst, time_span);
         }
@@ -82,8 +81,8 @@ public:
     {
         std::cout << "periodic (3) <= first homoclinic covering\n";
 
-        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = m_periodic_orbit_coordsys.at(3);
-        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = *( m_homoclinic_orbit_coordsys.begin() );
+        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = this->m_periodic_orbit_coordsys.at(3);
+        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = *( this->m_homoclinic_orbit_coordsys.begin() );
         const ScalarType time_span = check_covering_relation_backward(coordsys_src, coordsys_dst);
         simple_collision_avoidance_check(coordsys_src, coordsys_dst, time_span);
     }
@@ -93,7 +92,7 @@ public:
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void parallelogram_covering_derivative_check()
     {
-        const ScalarType L = m_basic_objects.m_parallelogram_coverings_parameters.L;
+        const ScalarType L = this->m_basic_objects.m_parallelogram_coverings_parameters.L;
 
         MapT eta = AuxiliaryFunctions<MapT>::eta( L );
         MapT eta_inverse = AuxiliaryFunctions<MapT>::eta( -L );
@@ -106,12 +105,12 @@ public:
             
             G_Map<MapT> poincare
             {
-                m_basic_objects.m_vf_reg_pos2,
-                m_basic_objects.m_hamiltonian_reg2,
-                m_basic_objects.m_order,
-                m_periodic_orbit_coordsys.at(first),
-                m_periodic_orbit_coordsys.at(second),
-                m_gain_factor
+                this->m_basic_objects.m_vf_reg_pos2,
+                this->m_basic_objects.m_hamiltonian_reg2,
+                this->m_basic_objects.m_order,
+                this->m_periodic_orbit_coordsys.at(first),
+                this->m_periodic_orbit_coordsys.at(second),
+                this->m_gain_factor
             };
 
             CapdUtils::CompositeMap<MapT, MapT&, decltype(poincare)&, MapT&> aligned_poincare
@@ -146,9 +145,9 @@ public:
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void parallelogram_covering_beginning_check()
     {
-        const ScalarType L = m_basic_objects.m_parallelogram_coverings_parameters.L;
-        const ScalarType b0 = m_basic_objects.m_parallelogram_coverings_parameters.b0;
-        const ScalarType a0 = m_basic_objects.m_parallelogram_coverings_parameters.a0;
+        const ScalarType L = this->m_basic_objects.m_parallelogram_coverings_parameters.L;
+        const ScalarType b0 = this->m_basic_objects.m_parallelogram_coverings_parameters.b0;
+        const ScalarType a0 = this->m_basic_objects.m_parallelogram_coverings_parameters.a0;
 
         EXPECT_TRUE(0 < a0);
         EXPECT_TRUE(a0 < b0);
@@ -158,17 +157,17 @@ public:
         MapT eta_inverse = AuxiliaryFunctions<MapT>::eta( -L );
         MapT J = AuxiliaryFunctions<MapT>::J();
 
-        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = m_periodic_orbit_coordsys.at(3);
-        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = *( m_homoclinic_orbit_coordsys.begin() );
+        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_src = this->m_periodic_orbit_coordsys.at(3);
+        const CapdUtils::LocalCoordinateSystem<MapT> coordsys_dst = *( this->m_homoclinic_orbit_coordsys.begin() );
 
         G_Map<MapT> poincare
         {
-            m_basic_objects.m_vf_reg_neg2,
-            m_basic_objects.m_hamiltonian_reg2,
-            m_basic_objects.m_order,
+            this->m_basic_objects.m_vf_reg_neg2,
+            this->m_basic_objects.m_hamiltonian_reg2,
+            this->m_basic_objects.m_order,
             coordsys_dst,
             coordsys_src,
-            m_gain_factor
+            this->m_gain_factor
         };
 
         CapdUtils::CompositeMap<MapT,
@@ -196,14 +195,14 @@ public:
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void collision_manifold_derivative_check()
     {
-        const ScalarType L = m_basic_objects.m_parallelogram_coverings_parameters.L;
+        const ScalarType L = this->m_basic_objects.m_parallelogram_coverings_parameters.L;
 
         MapT eta = AuxiliaryFunctions<MapT>::eta( L );
 
         LocalPoincare4_Constraint_Spec<MapT> map_E0
         {
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
-            std::ref( m_periodic_orbit_coordsys.at(0) )
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
+            std::ref( this->m_periodic_orbit_coordsys.at(0) )
         };
 
         MatrixType out(4, 2);
@@ -211,9 +210,9 @@ public:
 
         print_var(out);
 
-        CapdUtils::AffineMap<MapT> map_L0 { m_periodic_orbit_coordsys.at(0) };
+        CapdUtils::AffineMap<MapT> map_L0 { this->m_periodic_orbit_coordsys.at(0) };
 
-        MapT& map_C = m_basic_objects.m_collision_condition;
+        MapT& map_C = this->m_basic_objects.m_collision_condition;
 
         CapdUtils::CompositeMap<MapT,
             decltype(eta)&,
@@ -227,7 +226,7 @@ public:
             );
 
         MatrixType der(3, 2);
-        map_chi(N * m_gain_factor, der);
+        map_chi(N * this->m_gain_factor, der);
         const ScalarType fx = der(3,1);
         const ScalarType fy = der(3,2);
 
@@ -250,12 +249,12 @@ private:
     {
         G_Map<MapT> f
         {
-            std::ref(m_basic_objects.m_vf_reg_pos2),
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
-            m_basic_objects.m_order,
+            std::ref(this->m_basic_objects.m_vf_reg_pos2),
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
+            this->m_basic_objects.m_order,
             coordsys_src,
             coordsys_dst,
-            m_gain_factor,
+            this->m_gain_factor,
             specialized
         };
 
@@ -269,11 +268,11 @@ private:
         // check that image is properly covered by its coordinate system
         LocalPoincare4_Constraint<MapT> extension_to_4_dst
         {
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
             std::ref(coordsys_dst)
         };
 
-        extension_to_4_dst(cr.get_img() * m_gain_factor);
+        extension_to_4_dst(cr.get_img() * this->m_gain_factor);
         // print_var( extension_to_4_dst(img) );
 
         return time_span;
@@ -289,12 +288,12 @@ private:
     {
         G_Map<MapT> f
         {
-            std::ref(m_basic_objects.m_vf_reg_neg2),
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
-            m_basic_objects.m_order,
+            std::ref(this->m_basic_objects.m_vf_reg_neg2),
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
+            this->m_basic_objects.m_order,
             coordsys_dst,
             coordsys_src,
-            m_gain_factor
+            this->m_gain_factor
         };
 
         MapT J = AuxiliaryFunctions<MapT>::J();
@@ -315,11 +314,11 @@ private:
         // check that image is properly covered by its coordinate system
         LocalPoincare4_Constraint<MapT> extension_to_4_dst
         {
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
             std::ref(coordsys_dst)
         };
 
-        extension_to_4_dst(cr.get_img() * m_gain_factor);
+        extension_to_4_dst(cr.get_img() * this->m_gain_factor);
         // print_var( extension_to_4_dst(img) );
 
         return time_span;
@@ -332,64 +331,57 @@ private:
     {
         LocalPoincare4_Constraint<MapT> extension_to_4_src
         {
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
             std::ref(coordsys_src)
         };
 
         // check that image is properly covered by its coordinate system
         LocalPoincare4_Constraint<MapT> extension_to_4_dst
         {
-            std::ref(m_basic_objects.m_hamiltonian_reg2),
+            std::ref(this->m_basic_objects.m_hamiltonian_reg2),
             std::ref(coordsys_dst)
         };
 
         CapdUtils::MaxNorm<MapT> norm {};
 
-        const VectorType expected_collision = m_basic_objects.m_parameters.get_initial_point();
+        const VectorType expected_collision = this->m_basic_objects.m_parameters.get_initial_point();
         if ( norm(coordsys_src.get_origin() - expected_collision) < norm(coordsys_dst.get_origin() - expected_collision) )
         {
             G_Map<MapT> f_pos
             {
-                std::ref(m_basic_objects.m_vf_reg_pos2),
-                std::ref(m_basic_objects.m_hamiltonian_reg2),
-                m_basic_objects.m_order,
+                std::ref(this->m_basic_objects.m_vf_reg_pos2),
+                std::ref(this->m_basic_objects.m_hamiltonian_reg2),
+                this->m_basic_objects.m_order,
                 coordsys_src,
                 coordsys_dst,
-                m_gain_factor
+                this->m_gain_factor
             };
 
             SolutionCurveWithConditionCheck<MapT> solution_curve {};
             f_pos(N, time_span, solution_curve);
 
-            bool const solution_curve_condition = solution_curve.is_condition_never_satisfied( m_basic_objects.m_collision_condition );
+            bool const solution_curve_condition = solution_curve.is_condition_never_satisfied( this->m_basic_objects.m_collision_condition );
             EXPECT_TRUE(solution_curve_condition);
         }
         else
         {
             G_Map<MapT> f_neg
             {
-                std::ref(m_basic_objects.m_vf_reg_neg2),
-                std::ref(m_basic_objects.m_hamiltonian_reg2),
-                m_basic_objects.m_order,
+                std::ref(this->m_basic_objects.m_vf_reg_neg2),
+                std::ref(this->m_basic_objects.m_hamiltonian_reg2),
+                this->m_basic_objects.m_order,
                 coordsys_dst,
                 coordsys_src,
-                m_gain_factor
+                this->m_gain_factor
             };
 
             SolutionCurveWithConditionCheck<MapT> solution_curve {};
             f_neg(N, time_span, solution_curve);
 
-            bool const solution_curve_condition = solution_curve.is_condition_never_satisfied( m_basic_objects.m_collision_condition );
+            bool const solution_curve_condition = solution_curve.is_condition_never_satisfied( this->m_basic_objects.m_collision_condition );
             EXPECT_TRUE(solution_curve_condition);
         }
     }
-
-    Pcr3bp::RegBasicObjects<MapT> m_basic_objects {};
-
-    const std::vector<Coordsys> m_periodic_orbit_coordsys;
-    const std::vector<Coordsys> m_homoclinic_orbit_coordsys;
-
-    const ScalarType m_gain_factor { 75e-11 };
 };
 
 }
