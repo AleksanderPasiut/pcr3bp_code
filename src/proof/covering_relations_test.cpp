60afb26ebe54dbda2b9c053392da2ccee8a8eee8
Aleksander Pasiut <apasiut@protonmail.com>
2024-01-20 22:18:46 +0100

    add descriptions for the proofs

--- src/proof/covering_relations_test.cpp
+++ src/proof/covering_relations_test.cpp
@@ -4,6 +4,14 @@
 
 #include "covering_relations_test.hpp"
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! @brief Part of interval arithmetic validation of the proof of Theorem 8, where we assert the existence of covering
+//!        relations N_4 => N_5 => ... => N_K
+//!
+//!        In this computation we also perform a part of interval arithmetic validation of the proof of Lemma 8, where we
+//!        assert that the trajectories shadowing the covering relations described above, do not intersect with the collision
+//!        manifold.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 TEST(Pcr3bp_proof, homoclinic_coverings)
 {
     using namespace Pcr3bpProof;
@@ -15,6 +23,14 @@ TEST(Pcr3bp_proof, homoclinic_coverings)
     test.check_homoclinic_coverings();
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! @brief Part of interval arithmetic validation of the proof of Theorem 8, where we assert the existence of covering
+//!        relations N_0 => N_1 => N_2
+//!
+//!        In this computation we also perform a part of interval arithmetic validation of the proof of Lemma 8, where we
+//!        assert that the trajectories shadowing the covering relations described above, do not intersect with the collision
+//!        manifold outside of h-set N_0.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 TEST(Pcr3bp_proof, periodic_coverings)
 {
     using namespace Pcr3bpProof;
@@ -26,6 +42,14 @@ TEST(Pcr3bp_proof, periodic_coverings)
     test.check_periodic_coverings();
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! @brief Part of interval arithmetic validation of the proof of Theorem 8, where we assert the existence of covering
+//!        relation N_3 <= N_4
+//!
+//!        In this computation we also perform a part of interval arithmetic validation of the proof of Lemma 8, where we
+//!        assert that the trajectories shadowing the covering relation described above, do not intersect with the collision
+//!        manifold.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 TEST(Pcr3bp_proof, jump_coverings)
 {
     using namespace Pcr3bpProof;
@@ -37,6 +61,14 @@ TEST(Pcr3bp_proof, jump_coverings)
     test.check_jump_coverings();
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! @brief Interval arithmetic validation of the proof of Lemma 11, where we assert the existence of covering relation
+//!        SR_1 <= N_4
+//!
+//!        In this computation we also perform a part of interval arithmetic validation of the proof of Lemma 8, where we
+//!        assert that the trajectories shadowing the covering relation described above, do not intersect with the collision
+//!        manifold.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 TEST(Pcr3bp_proof, parallelogram_coverings_beginning)
 {
     using namespace Pcr3bpProof;
