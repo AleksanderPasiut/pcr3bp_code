cbdb95ad686bad2c4d66d0b1b65d8ec155e355ca
Aleksander Pasiut <apasiut@protonmail.com>
2024-01-21 11:43:32 +0100

    split psi0 coefficients from psi0 constraint spec

--- src/tools/local_poincare4_constraint_spec.hpp
+++ src/tools/local_poincare4_constraint_spec.hpp
@@ -18,6 +18,107 @@ namespace Pcr3bpProof
 {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! @brief Compute coefficients d1 and d2 for psi0 map
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename MapT>
+class Psi0_Coefficients
+{
+public:
+    using ScalarType = typename MapT::ScalarType;
+    using VectorType = typename MapT::VectorType;
+    using MatrixType = typename MapT::MatrixType;
+
+    static Psi0_Coefficients& get(const CapdUtils::LocalCoordinateSystem<MapT>& src_coordsys)
+    {
+        static Psi0_Coefficients s_instance { src_coordsys };
+        return s_instance;
+    }
+    
+    MapT& get_internal_map_ref() noexcept
+    {
+        return m_internal_map;
+    }
+
+    const ScalarType& get_d1() const noexcept
+    {
+        return m_d1;
+    }
+
+    const ScalarType& get_d2() const noexcept
+    {
+        return m_d2;
+    }
+
+    const CapdUtils::LocalCoordinateSystem<MapT>& get_src_coordsys() const noexcept
+    {
+        return m_src_coordsys_2_dim;
+    }
+
+private:
+    Psi0_Coefficients(const CapdUtils::LocalCoordinateSystem<MapT>& src_coordsys) : m_src_coordsys_4_dim(src_coordsys)
+    {}
+
+    static CapdUtils::LocalCoordinateSystem<MapT> create_src_coordsys(
+        MapT& internal_map,
+        const CapdUtils::LocalCoordinateSystem<MapT>& dst_coordsys,
+        ScalarType& d1,
+        ScalarType& d2)
+    {
+        const VectorType unstable_dir = CapdUtils::Extract<MapT>::get_vvector(dst_coordsys.get_directions_matrix(), 1);
+        print_var(unstable_dir);
+
+        MatrixType dd(4, 2);
+        internal_map( VectorType(2), dd );
+
+        MatrixType dd2(2,4);
+        for (int i = 1; i <= 2; ++i)
+        {
+            for (int j = 1; j <= 4; ++j)
+            {
+                dd2(i,j) = dd(j,i);
+            }
+        }
+
+        const MatrixType ddx = dd2 * dd;
+
+        const VectorType unstable_dir_reg = dd2 * unstable_dir;
+        const VectorType d_coeff = CapdUtils::gauss<MapT>(ddx, unstable_dir_reg);
+
+        d1 = d_coeff[0];
+        d2 = d_coeff[1];
+
+        print_var(d1);
+        print_var(d2);
+
+        const VectorType origin = VectorType(2);
+        MatrixType directions(2,2);
+        directions(1,1) =  d1;
+        directions(1,2) =  d1;
+        directions(2,1) =  d2;
+        directions(2,2) = -d2;
+
+        return CapdUtils::LocalCoordinateSystem<MapT>(origin, directions);
+    }
+
+    Pcr3bp::RegBasicObjects<MapT> m_basic_objects {};
+
+    MapT m_internal_map
+    {
+        Psi0_specialized<MapT>::create( m_basic_objects.m_h0, m_basic_objects.m_setup )
+    };
+
+    ScalarType m_d1 {};
+    ScalarType m_d2 {};
+
+    const CapdUtils::LocalCoordinateSystem<MapT> m_src_coordsys_4_dim;
+
+    const CapdUtils::LocalCoordinateSystem<MapT> m_src_coordsys_2_dim
+    {
+        create_src_coordsys(m_internal_map, m_src_coordsys_4_dim, m_d1, m_d2)
+    };
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //! @brief Implementation of the psi function (specialized for psi0)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 template<typename MapT>
@@ -32,7 +133,7 @@ public:
         MapT& constraint,
         const CapdUtils::LocalCoordinateSystem<MapT>& src_coordsys)
             : LocalPoincare4_Constraint_Base<MapT>(constraint, src_coordsys)
-            , m_dst_coordsys(src_coordsys)
+            , m_src_coordsys_4_dim(src_coordsys)
     {}
 
     VectorType operator() (const VectorType& vec) override
@@ -56,68 +157,24 @@ public:
     }
 
 private:
-    static CapdUtils::LocalCoordinateSystem<MapT> create_src_coordsys(
-        MapT& internal_map,
-        const CapdUtils::LocalCoordinateSystem<MapT>& dst_coordsys)
+    const CapdUtils::LocalCoordinateSystem<MapT> m_src_coordsys_4_dim;
+
+    MapT& m_internal_map
     {
-        const VectorType unstable_dir = CapdUtils::Extract<MapT>::get_vvector(dst_coordsys.get_directions_matrix(), 1);
-        print_var(unstable_dir);
-
-        MatrixType dd(4, 2);
-        internal_map( VectorType(2), dd );
-
-        print_var(dd);
-
-        MatrixType dd2(2,4);
-        for (int i = 1; i <= 2; ++i)
-            for (int j = 1; j <= 4; ++j)
-                dd2(i,j) = dd(j,i);
-
-        print_var(dd2);
-
-        MatrixType ddx = dd2 * dd;
-
-        print_var(ddx);
-
-        const VectorType unstable_dir_reg = dd2 * unstable_dir;
-        const VectorType d_coeff = CapdUtils::gauss<MapT>(ddx, unstable_dir_reg);
-
-        const ScalarType d1 = d_coeff[0];
-        const ScalarType d2 = d_coeff[1];
-
-        print_var(d1);
-        print_var(d2);
-
-        const VectorType origin = VectorType(2);
-        MatrixType directions(2,2);
-        directions(1,1) =  d1;
-        directions(1,2) =  d1;
-        directions(2,1) =  d2;
-        directions(2,2) = -d2;
-
-        return CapdUtils::LocalCoordinateSystem<MapT>(origin, directions);
-    }
-
-    Pcr3bp::RegBasicObjects<MapT> m_basic_objects {};
-
-    MapT m_internal_map
-    {
-        Psi0_specialized<MapT>::create( m_basic_objects.m_h0, m_basic_objects.m_setup )
+        Psi0_Coefficients<MapT>::get(m_src_coordsys_4_dim).get_internal_map_ref()
     };
 
-    const CapdUtils::LocalCoordinateSystem<MapT> m_dst_coordsys;
-
-    const CapdUtils::LocalCoordinateSystem<MapT> m_src_coordsys
+    const CapdUtils::LocalCoordinateSystem<MapT> m_src_coordsys_2_dim
     {
-        create_src_coordsys(m_internal_map, m_dst_coordsys)
+        Psi0_Coefficients<MapT>::get(m_src_coordsys_4_dim).get_src_coordsys()
     };
     
     CapdUtils::LocalMap<MapT,
         decltype(m_internal_map)&> m_local_map
     {
         std::ref(m_internal_map),
-        std::ref(m_src_coordsys),
-        std::ref(m_dst_coordsys)
+        std::ref(m_src_coordsys_2_dim),
+        std::ref(m_src_coordsys_4_dim)
     };
 };
 
