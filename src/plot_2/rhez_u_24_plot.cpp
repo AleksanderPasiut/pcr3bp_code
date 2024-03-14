2b373503577f4b8b47489681d459279f68249228
Aleksander Pasiut <apasiut@protonmail.com>
2024-03-14 20:00:44 +0100

    move hset parameters list loading to sep. file

--- src/plot_2/rhez_u_24_plot.cpp
+++ src/plot_2/rhez_u_24_plot.cpp
@@ -26,47 +26,10 @@
 #include "plot_2/objects/collision_manifold.hpp"
 
 #include "plot_2/objects/object_with_params.hpp"
+#include "load_hset_parameters_list.hpp"
 
 namespace Pcr3bpProof
 {
-
-std::list<CapdUtils::HsetParameters> load_hset_parameters_list()
-{
-    auto load_hset_parameters_list_file = [](std::string file_name) -> std::list<CapdUtils::HsetParameters>
-    {
-        std::ifstream ifs(file_name);
-        if (ifs)
-        {
-            return CapdUtils::deserialize_hset_parameters_list(ifs);
-        }
-
-        throw std::logic_error("Failed to find hset parameters file!");
-    };
-    auto homoclinic_hset_parameters_list = load_hset_parameters_list_file("homoclinic_coverings_hset_parameters.csv");
-    auto periodic_hset_parameters_list = load_hset_parameters_list_file("periodic_coverings_hset_parameters.csv");
-    auto jump_hset_parameters_list = load_hset_parameters_list_file("jump_coverings_hset_parameters.csv");
-
-    CapdUtils::HsetParameters periodic_orbit_2_arg_hset
-    {
-        .type = CapdUtils::HsetType::Argument,
-        .coordsys_origin = periodic_hset_parameters_list.rbegin()->coordsys_origin,
-        .coordinates = periodic_hset_parameters_list.begin()->coordinates
-    };
-
-    std::list<CapdUtils::HsetParameters> hset_parameters_list {};
-    hset_parameters_list.splice(hset_parameters_list.end(), homoclinic_hset_parameters_list);
-    hset_parameters_list.splice(hset_parameters_list.end(), periodic_hset_parameters_list);
-    hset_parameters_list.splice(hset_parameters_list.end(), jump_hset_parameters_list);
-
-    hset_parameters_list.emplace_back(periodic_orbit_2_arg_hset);
-
-
-    // for (auto& hp : hset_parameters_list)
-    // {
-    //     hp.serialize(std::cout);
-    // }
-    return hset_parameters_list;
-}
 
 class CoreInterior : CoreInteriorBaseRhez_u_24
 {
